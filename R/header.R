cdeed33668c355ccfcd2b1b705aef06860f034e0
Duncan Temple Lang <duncan@wald.ucdavis.edu>
2008-10-14 10:19:15 +0000

    version 0.91-0

--- R/header.R
+++ R/header.R
@@ -1,37 +1,68 @@
 parseHTTPHeader =
   #
   # 
+  #
+  # Reads the lines from the HTTP response 
+  # header and converts them into a
+  # name=value  collection as a character vector.
+  #
   # returns a named list of the fields. Allows duplicates.
-  #
-  #
-  # This is taken from SSOAP. Need to remove the version there which
-  # doesn't do the strsplit() if lines is a single string.
-  #
-function (lines) 
+function(lines)
 {
-    if (length(lines) < 1) 
-        return(NULL)
+ if(length(lines) < 1)
+   return(NULL)
 
-    if(length(lines) == 1)
-      lines = strsplit(lines, "\r\n")[[1]]
+ if(length(lines) == 1)
+    lines = strsplit(lines, "\r\n")[[1]]
+ 
+ status = lines[1]
 
-    status = lines[1]
-    lines = lines[-c(1, length(lines))]
-    lines = gsub("\r\n", "", lines)
-    if (FALSE) {
-        header = lines[-1]
-        header <- read.dcf(textConnection(header))
-    }
-    else {
-        els <- sapply(lines, function(x) strsplit(x, ":[ ]*"))
-        header <- lapply(els, function(x) x[2])
-        names(header) <- sapply(els, function(x) x[1])
-    }
-    els <- strsplit(status, " ")[[1]]
-    header[["status"]] <- as.integer(els[2])
-    header[["statusMessage"]] <- els[3]
-    header
+   # Get the status and if it is 100, then throw away the first 2 lines
+   #  e.g. HTTP/1.1 100 Continue
+   #       \r\n
+ st = getStatus(status)
+ if(st[["status"]] == 100) {
+     # Have to worry about whether we just get a 100 and nothing else
+     # and so lines[3] won't exist.
+   status = lines[3]
+   lines = lines[-(1:2)]
+ }
+ 
+ lines = lines[-c(1, length(lines))]
+ lines = gsub("\r\n$","", lines)  # was \r\n\0$
+
+ if(FALSE) { # Doesn't behave. Duplicates!
+   header = lines[-1]
+   header <- read.dcf(textConnection(header))
+ } else {
+   els <- sapply(lines, function(x) strsplit(x, ": *"))
+   header <- sapply(els, function(x) x[2]) #XX what if more than 2 els.
+   names(header) <- sapply(els, function(x) x[1]) 
+ }
+
+ st = getStatus(status)
+ header[["status"]] = st[["status"]]
+ header[["statusMessage"]] = st[["message"]] 
+ 
+ header
 }
+
+
+getStatus =
+  #
+  # Reads the first line of the HTTP header, e.g of the form
+  #   HTTP/1.1 200 OK
+  # or
+  #   HTTP/1.1 404 Not Found
+  # and returns
+  #   list(status = 404, message = "Not Found")
+function(status)
+{
+ els <- strsplit(status, " ")[[1]] 
+ list(status = as.integer(els[2]), message = paste(els[-c(1,2)], collapse = " "))
+}
+
+
 
 
 getEncoding =
