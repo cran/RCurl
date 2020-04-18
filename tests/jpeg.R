8a0f1af6437885bd24ad954daf9842dbbef48ce9
CRAN Team <CRAN@r-project.org>
2020-04-18 02:44:52 +0000

    version 1.98-1.2

--- tests/jpeg.R
+++ tests/jpeg.R
@@ -2,9 +2,9 @@ library(RCurl)
 
 url = "http://www.omegahat.net/Rcartogram/demo.jpg"
 header = RCurl:::basicTextGatherer()
-ans = getBinaryURL(url, headerfunction = header$update)
+ans = try(getBinaryURL(url, headerfunction = header$update))
 
 
 url = "http://eeyore.ucdavis.edu/stat141/data/Elections/NYExitData.rda"
 header = RCurl:::basicTextGatherer()
-ans = getBinaryURL(url, headerfunction = header$update)
+ans = try(getBinaryURL(url, headerfunction = header$update))
